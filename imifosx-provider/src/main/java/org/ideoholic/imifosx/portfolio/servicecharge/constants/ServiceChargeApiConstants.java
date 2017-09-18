63db8a35edbfe76ffc0bea3284bf5fa38c8f3edb
Reehan <reehan.lalkhan@gmail.com>
2017-09-18 23:30:31 +0530

    Introducing constant MathContext for BigDecimal round-off usage

--- imifosx-provider/src/main/java/org/ideoholic/imifosx/portfolio/servicecharge/constants/ServiceChargeApiConstants.java
+++ imifosx-provider/src/main/java/org/ideoholic/imifosx/portfolio/servicecharge/constants/ServiceChargeApiConstants.java
@@ -2,6 +2,7 @@
 package org.ideoholic.imifosx.portfolio.servicecharge.constants;
 
 import java.math.BigDecimal;
+import java.math.MathContext;
 
 public interface ServiceChargeApiConstants {
 
@@ -10,5 +11,6 @@ public interface ServiceChargeApiConstants {
 	String SERVICE_CHARGE_REST_CALL = SLASH + serviceChargeRESTName;
 	BigDecimal ONE_THOUSAND_TWO_HUNDRED = new BigDecimal("1200");
 	BigDecimal HUNDRED = new BigDecimal("100");
+	MathContext MATHCONTEXT_2 = new MathContext(2);
 
 }
