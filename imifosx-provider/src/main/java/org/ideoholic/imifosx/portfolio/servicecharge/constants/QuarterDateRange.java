2a2db3c30a8eace5c006e8bd8435c7a6ec345de6
Reehan <reehan.lalkhan@gmail.com>
2017-10-09 21:46:32 +0530

    Adding id to QuarterDateRange for storing and retrieving from DB

--- imifosx-provider/src/main/java/org/ideoholic/imifosx/portfolio/servicecharge/constants/QuarterDateRange.java
+++ imifosx-provider/src/main/java/org/ideoholic/imifosx/portfolio/servicecharge/constants/QuarterDateRange.java
@@ -2,14 +2,17 @@ package org.ideoholic.imifosx.portfolio.servicecharge.constants;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 import org.ideoholic.imifosx.accounting.journalentry.api.DateParam;
 import org.ideoholic.imifosx.infrastructure.core.service.DateUtils;
 
 public enum QuarterDateRange {
-	Q1("01 Jan ", "31 Mar "), Q2("01 Apr ", "30 Jun "), Q3("01 Jul ", "30 Sep "), Q4("30 Sep ", "31 Dec ");
+	Q1(1, "01 Jan ", "31 Mar "), Q2(2, "01 Apr ", "30 Jun "), Q3(3, "01 Jul ", "30 Sep "), Q4(4, "30 Sep ", "31 Dec ");
 
+	private final Integer id;
 	private final String fromDate;
 	private final String toDate;
 	// TODO: Explore using -
@@ -17,9 +20,14 @@ public enum QuarterDateRange {
 	// private final DateRange dateRange;
 	private final String dateFormatString = "dd MMMM yyyy";
 
-	private QuarterDateRange(final String fromDate, final String toDate) {
+	private QuarterDateRange(final Integer id, final String fromDate, final String toDate) {
+		this.id = id;
 		this.fromDate = fromDate;
 		this.toDate = toDate;
+	}
+
+	public Integer getId() {
+		return id;
 	}
 
 	public String getDateFormatString() {
@@ -68,15 +76,15 @@ public enum QuarterDateRange {
 		Calendar c = Calendar.getInstance(Locale.getDefault());
 		int month = c.get(Calendar.MONTH);
 
-		q = (month >= Calendar.JANUARY && month <= Calendar.MARCH) ? Q1
-				: (month >= Calendar.APRIL && month <= Calendar.JUNE) ? Q2 : (month >= Calendar.JULY && month <= Calendar.SEPTEMBER) ? Q3 : Q4;
+		q = (month >= Calendar.JANUARY && month <= Calendar.MARCH) ? Q1 : (month >= Calendar.APRIL && month <= Calendar.JUNE) ? Q2
+				: (month >= Calendar.JULY && month <= Calendar.SEPTEMBER) ? Q3 : Q4;
 		return q;
 	}
-	
+
 	public String getFormattedFromDateString() {
 		return DateUtils.formatToSqlDate(getFromDateForCurrentYear());
 	}
-	
+
 	public String getFormattedToDateString() {
 		return DateUtils.formatToSqlDate(getToDateForCurrentYear());
 	}
@@ -84,6 +92,18 @@ public enum QuarterDateRange {
 	@Override
 	public String toString() {
 		return name().toString();
+	}
+
+	private static final Map<Integer, QuarterDateRange> intToEnumMap = new HashMap<>();
+	static {
+		for (final QuarterDateRange type : QuarterDateRange.values()) {
+			intToEnumMap.put(type.getId(), type);
+		}
+	}
+
+	public static QuarterDateRange fromInt(final int i) {
+		final QuarterDateRange type = intToEnumMap.get(Integer.valueOf(i));
+		return type;
 	}
 
 }
