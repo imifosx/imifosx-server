40f4e2f7c9a72481f98c3f258b656be96dae16e2
Reehan <reehan.lalkhan@gmail.com>
2018-01-20 17:58:47 +0530

    Changed equals to compareTo for comparision independent of scale

--- imifosx-provider/src/main/java/org/ideoholic/imifosx/portfolio/servicecharge/util/ServiceChargeOperationUtils.java
+++ imifosx-provider/src/main/java/org/ideoholic/imifosx/portfolio/servicecharge/util/ServiceChargeOperationUtils.java
@@ -30,7 +30,7 @@ public class ServiceChargeOperationUtils {
 		if (operand == null) {
 			return BigDecimal.ONE;
 		}
-		if (divisor != null && !divisor.equals(BigDecimal.ZERO)) {
+		if (divisor != null && (divisor.compareTo(BigDecimal.ZERO) == 0)) {
 			operand = operand.divide(divisor, RoundingMode.HALF_UP);
 		}
 		return operand;
