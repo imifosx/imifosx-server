e02bfa2e6f3aff6c94d16c133f0bade53557de3b
Musaib Asrar <musaibasrar@gmail.com>
2018-05-22 10:25:18 +0530

    average savings deposit calculation

---
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/servicecharge/saving/SavingAccountsCalculationPlatformService.java
@@ -0,0 +1,14 @@
+package org.apache.fineract.portfolio.servicecharge.saving;
+
+import java.math.BigDecimal;
+
+public interface SavingAccountsCalculationPlatformService {
+
+    /**
+     * Saving Accounts average savings calculation method for a given period
+     * 
+     * @return Average savings
+     */
+    BigDecimal calculateAverageSavings();
+
+}
