2f1d0401eaaf6308175581a06fec2102ec6bd815
Reehan <reehan.lalkhan@gmail.com>
2018-06-06 12:06:05 +0530

    Improving ServiceChargeException to contain Enum with constants to handle different type of exceptions

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/servicecharge/exception/ServiceChargeException.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/servicecharge/exception/ServiceChargeException.java
@@ -18,11 +18,37 @@
  */
 package org.apache.fineract.portfolio.servicecharge.exception;
 
-import org.apache.fineract.infrastructure.core.exception.AbstractPlatformResourceNotFoundException;
+import org.apache.fineract.infrastructure.core.exception.AbstractPlatformDomainRuleException;
 
-public class ServiceChargeException extends AbstractPlatformResourceNotFoundException {
+public class ServiceChargeException extends AbstractPlatformDomainRuleException {
+
+    public static enum SERVICE_CHARGE_EXCEPTION_REASON {
+        SC_CALCULATION_EXCEPTION, SC_INSTALLMENT_ADJUSTMENT_EXCEPTION;
+
+        public String errorMessage() {
+            if (name().toString().equalsIgnoreCase("SC_CALCULATION_EXCEPTION")) {
+                return "Service Charge Exception occurred when calculating Service Charge Final Sheet values";
+            } else if (name().toString().equalsIgnoreCase("SC_INSTALLMENT_ADJUSTMENT_EXCEPTION")) {
+                return "This loan charge has already been waived";
+            }
+            return name().toString();
+        }
+
+        public String errorCode() {
+            if (name().toString().equalsIgnoreCase("SC_CALCULATION_EXCEPTION")) {
+                return "error.msg.charge.id.invalid";
+            } else if (name().toString().equalsIgnoreCase("SC_INSTALLMENT_ADJUSTMENT_EXCEPTION")) {
+                return "error.msg.loan.charge.already.waived";
+            }
+            return name().toString();
+        }
+    }
 
     public ServiceChargeException(final Long id) {
-        super("error.msg.charge.id.invalid", "Service Charge Exception occurred when calculating Service Charge values", id);
+        super("error.msg.servicecharge.id.generic.exception", "Generic Exception used when calculating Service Charge values", id);
+    }
+    
+    public ServiceChargeException(final SERVICE_CHARGE_EXCEPTION_REASON reason, final Long loanChargeId) {
+        super(reason.errorCode(), reason.errorMessage(), loanChargeId);
     }
 }
