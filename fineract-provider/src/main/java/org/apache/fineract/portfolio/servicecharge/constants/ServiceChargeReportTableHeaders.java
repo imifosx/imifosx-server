587a6fbe234479fcdfdfbce1eadbe84a51f9f762
Reehan <reehan.lalkhan@gmail.com>
2018-06-07 16:24:20 +0530

    Formatting changes to ServiceChargeReportTableHeaders to align with Mifos formatter and better view

--- fineract-provider/src/main/java/org/apache/fineract/portfolio/servicecharge/constants/ServiceChargeReportTableHeaders.java
+++ fineract-provider/src/main/java/org/apache/fineract/portfolio/servicecharge/constants/ServiceChargeReportTableHeaders.java
@@ -23,71 +23,69 @@ import java.util.Map;
 
 public enum ServiceChargeReportTableHeaders {
 	
-	SUBTOTAL(1, "Sub Total"),
-	ALLOCATION_I_OVERHEADS(2, "Allocation-I (Overheads)"),
-	SUBTOTAL_ALLOCATION(3, "Sub Total after Overheads Allocation"),
-	ALLOCATION_II_MOBILIZATION(4, "Allocation-II (Mobilization Cost)"),
+    SUBTOTAL(1, "Sub Total"),
+    ALLOCATION_I_OVERHEADS(2, "Allocation-I (Overheads)"),
+    SUBTOTAL_ALLOCATION(3, "Sub Total after Overheads Allocation"),
+    ALLOCATION_II_MOBILIZATION(4, "Allocation-II (Mobilization Cost)"),
     TOTAL_SEGREGATION_COST(5, "Total Activity-wise Segregated Cost"),
     LSCOST_ON_ACCOUNT_BF(6, "LS Cost on A/c BF"),
     TOTAL_MOBILIZATION(7, "Total Mobilisation Cost p.a."),
-	AVG_REPAYMENT(8, "Average OS DL Re.Months"),
-	MOBILIZATION_PERCENT(9, "Mobilisation Cost (%)"),
+    AVG_REPAYMENT(8, "Average OS DL Re.Months"),
+    MOBILIZATION_PERCENT(9, "Mobilisation Cost (%)"),
     LOAN_SERVICING_PA(10, "Loan Servicing Cost p.a."),
     TOTAL_LOANS(11, "Total No.of DL Loans for the Period"),
-	LOAN_SERVICING_PER_LOAN(12, "Loan Servicing Cost per Loan"),
+    LOAN_SERVICING_PER_LOAN(12, "Loan Servicing Cost per Loan"),
     TOTAL_REPAYMENT(13, "Total Repayment for the Period"),
     REPAYMENT_PER_100(14, "Repayment Cost per 100 Rupee of Repayment"),
     ANNUALIZED_COST_I(15, "Equivalent Annualized Cost (%) - I"),
     ANNUALIZED_COST_II(16, "Equivalent Annualized Cost (%) - II"),
     ANNUALIZED_COST_III(17, "Equivalent Annualized Cost (%) - III"),
     ANNUALIZED_COST_TOTAL(18, "Equivalent Annualized Cost (%) - Total"),
-	INVALID(100, "INVALID HEADER");
-	private final Integer value;
-	private final String code;
+    INVALID(100, "INVALID HEADER");
+    
+    private final Integer value;
+    private final String code;
 
-	private ServiceChargeReportTableHeaders(final Integer value,
-			final String code) {
-		this.value = value;
-		this.code = code;
-	}
+    private ServiceChargeReportTableHeaders(final Integer value, final String code) {
+        this.value = value;
+        this.code = code;
+    }
 
-	public Integer getValue() {
-		return this.value;
-	}
+    public Integer getValue() {
+        return this.value;
+    }
 
-	public String getCode() {
-		return this.code;
-	}
+    public String getCode() {
+        return this.code;
+    }
 
-	private static final Map<Integer, ServiceChargeReportTableHeaders> intToEnumMap = new HashMap<>();
-	private static int minValue;
-	private static int maxValue;
-	static {
-		int i = 0;
-		for (final ServiceChargeReportTableHeaders type : ServiceChargeReportTableHeaders
-				.values()) {
-			if (i == 0) {
-				minValue = type.value;
-			}
-			intToEnumMap.put(type.value, type);
-			if (minValue >= type.value) {
-				minValue = type.value;
-			}
-			if (maxValue < type.value) {
-				maxValue = type.value;
-			}
-			i = i + 1;
-		}
-	}
+    private static final Map<Integer, ServiceChargeReportTableHeaders> intToEnumMap = new HashMap<>();
+    private static int minValue;
+    private static int maxValue;
+    static {
+        int i = 0;
+        for (final ServiceChargeReportTableHeaders type : ServiceChargeReportTableHeaders.values()) {
+            if (i == 0) {
+                minValue = type.value;
+            }
+            intToEnumMap.put(type.value, type);
+            if (minValue >= type.value) {
+                minValue = type.value;
+            }
+            if (maxValue < type.value) {
+                maxValue = type.value;
+            }
+            i = i + 1;
+        }
+    }
 
-	public static ServiceChargeReportTableHeaders fromInt(final int i) {
-		final ServiceChargeReportTableHeaders type = intToEnumMap.get(Integer
-				.valueOf(i));
-		return type;
-	}
+    public static ServiceChargeReportTableHeaders fromInt(final int i) {
+        final ServiceChargeReportTableHeaders type = intToEnumMap.get(Integer.valueOf(i));
+        return type;
+    }
 
-	@Override
-	public String toString() {
-		return name().toString();
-	}
+    @Override
+    public String toString() {
+        return name().toString();
+    }
 }
